4ba0bb344d9d2c82d5b0701aeff41b3365448580
Sandeep Datta <datta.sandeep@gmail.com>
2016-04-23 18:33:23 +0530

    Updated example to demonstrate the use of the type_of!() macro.

--- examples/basic.rs
+++ examples/basic.rs
@@ -4,13 +4,19 @@ extern crate type_of;
 use type_of::*;
 
 fn main() {
-    print_type_of!(32.90);
-    print_type_of!([1, 2, 4]);
-    print_type_of!(vec![1, 2, 4]);
-    print_type_of!("123");
+
+    assert_eq!("f64", type_of!(32.90));
+    assert_eq!("[i32; 3]", type_of!([1, 2, 4]));
+    assert_eq!("std::vec::Vec<i32>", type_of!(vec![1, 2, 4]));
+    assert_eq!("&'static str", type_of!("123"));
 
     assert_eq!("32.90: f64", format_type_of!(32.90));
     assert_eq!("[1, 2, 4]: [i32; 3]", format_type_of!([1, 2, 4]));
     assert_eq!("vec!(1 , 2 , 4): std::vec::Vec<i32>", format_type_of!(vec![1, 2, 4]));
     assert_eq!("\"123\": &'static str", format_type_of!("123"));
+
+    print_type_of!(32.90);
+    print_type_of!([1, 2, 4]);
+    print_type_of!(vec![1, 2, 4]);
+    print_type_of!("123");
 }
